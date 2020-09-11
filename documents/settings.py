ed97c222c36fc6ea768994b52c504e238dda549f
Andrey Zhelezovskiy <azhele@gmail.com>
2020-09-11 13:52:02 +0300

    change db

--- documents/settings.py
+++ documents/settings.py
@@ -59,12 +59,8 @@ WSGI_APPLICATION = 'documents.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'documents',
-        'USER': 'azhele',
-        'PASSWORD': 'DocsDBpwd',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
